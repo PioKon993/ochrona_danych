4bf3335e5acd4c82d159296a8d3ebb2e734773a1
Piotr Kondratjew <piotr.kondratjew@gmail.com>
2018-12-03 22:30:44 +0100

    final version

--- zad2-klucze/WyborKluczy.py
+++ zad2-klucze/WyborKluczy.py
@@ -4,6 +4,21 @@ import os
 import sys
 import random
 
+def extended_euklides(a, n):
+
+    p0, p1, a0, n0 = 0, 1, a, n
+    q, r = n0 // a0, n0 % a0
+    while r:
+        t = p0 - (q * p1)
+
+        if t >= 0:
+            t = t % n
+        else:
+            t = n - ((-t) % n)
+        p0, p1, n0, a0 = p1, t, a0, r
+        q, r  = n0 // a0, n0 % a0
+    return p1
+
 def NWD(a,b):
     while True:
         r = a % b
@@ -11,7 +26,7 @@ def NWD(a,b):
             return b
         a, b = b, r
 
-def write_to_file(kind_of_key,values):
+def generate_key(kind_of_key,values):
     if (kind_of_key == 'public'):
         f = open('public.key', 'w')
         for x in range(0,len(values)):
@@ -24,17 +39,20 @@ def write_to_file(kind_of_key,values):
         f.close()
 
 if __name__ == '__main__':
-    p=random.randint(1,1000000000)
-    q=random.randint(1,1000000000)
+
+    #p=random.randint(1,1000000000)
+    #q=random.randint(1,1000000000)
+    p=random.randint(1,20)
+    q=random.randint(1,40)
     e=random.randint(1,100)
     n=p*q
     phi=(p-1)*(q-1)
-    print(p,q,e,n,phi)
+
     while NWD(e,phi)!=1:
         e=random.randint(1,100)
-    print(e,phi)
-    d=e*(phi-1)%phi
+
+    d=extended_euklides(e,phi)
     publicKey=[e,n]
     privateKey=[d,n]
-    write_to_file('public',publicKey)
-    write_to_file('private',privateKey)
+    generate_key('public', publicKey)
+    generate_key('private', privateKey)
